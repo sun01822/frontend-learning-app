4e31063a9f0427f31067355e70d0a8f696522e44
MDShariarHossainSun <mdshariarhossainsun01822@gmail.com>
2023-05-17 20:35:49 +0600

    Landing page added

--- components/home/About.jsx
+++ components/home/About.jsx
@@ -9,14 +9,14 @@ const About = () => {
             title: "Class",
         },
         {
-            image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzCrWmq0Q8Y5_PLr-y3EighNfdVK0gxa8r0A&usqp=CAU",
+            image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7JUQAzAbIzUT3IbwzvtDMErDlML7CFWDw4jrGilmTMObAfdmS0jgLPQUwnyCNuoXU0VM&usqp=CAU",
             bold: "Best",
-            title: "Live Class 2",
+            title: "Teachers",
         },
         {
-            image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzCrWmq0Q8Y5_PLr-y3EighNfdVK0gxa8r0A&usqp=CAU",
+            image: "https://economictimes.indiatimes.com/thumb/msid-90954525,width-1200,height-843,resizemode-4,imgsize-32884/online-earning-through-affiliate-marketing.jpg?from=mdr",
             bold: "Earn",
-            title: "Live Class 3",
+            title: "Money",
         },
     ]
     return (
@@ -27,20 +27,6 @@ const About = () => {
                 </div>
                 <p className="text-xl text-center">A modern and unique system to share experience and earn money</p>
             </div>
-            {/* <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10">
-                <Card
-                    imageUrl=""/>
-                 <Card
-                    imageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7JUQAzAbIzUT3IbwzvtDMErDlML7CFWDw4jrGilmTMObAfdmS0jgLPQUwnyCNuoXU0VM&usqp=CAU"/>
-                 <Card
-                    imageUrl="https://economictimes.indiatimes.com/thumb/msid-90954525,width-1200,height-843,resizemode-4,imgsize-32884/online-earning-through-affiliate-marketing.jpg?from=mdr"/>
-            </div>
-
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10">
-                    <h1 className="text-center text-3xl mt-10 font-bold"><span className="text-green-500 mr-1">Live</span>Class</h1>
-                    <h1 className="text-center text-3xl mt-10 font-bold"><span className="text-green-500 mr-1">Best</span>Teachers</h1>
-                    <h1 className="text-center text-3xl mt-10 font-bold"><span className="text-green-500 mr-1">Earn</span>Money</h1>
-            </div> */}
             <div className="grid md:grid-cols-3 gap-6 text-center mt-10">
                 {
                     cardData.map((item) => (
