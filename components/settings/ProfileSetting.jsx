4197031952bfa39f64932924a84f2bda6788a3f8
Al-Amin <76097180+alamin2731@users.noreply.github.com>
2023-06-25 20:00:49 +0600

    fixed alignment of ProfileSettings

--- components/settings/ProfileSetting.jsx
+++ components/settings/ProfileSetting.jsx
@@ -16,7 +16,7 @@ const ProfileSettings = () => {
                     <label className="label">
                         <span className="label-text">Name</span>
                     </label>
-                    <input type="text" placeholder="Md. Rakibuzzaman" readOnly className="input input-bordered w-full max-w-xs" />
+                    <input type="text" Value="Md. Rakibuzzaman" className="input input-bordered w-full max-w-xs" />
 
                     <label className="label">
                         <span className="label-text">Email</span>
@@ -31,13 +31,41 @@ const ProfileSettings = () => {
                     <button className="btn btn-sar btn-sm">Save</button>
 
                 </div>
+                <div className="study">
+                    <h3 className="text-xl font-semibold">Study</h3>
+                    <hr />
+                    <label className="label">
+                        <span className="label-text">I study at</span>
+                    </label>
+                    <input type="text" Value="Varendra University" className="input input-bordered w-full max-w-xs" />
+
+                    <label className="label">
+                        <span className="label-text">I study in </span>
+                    </label>
+                    <input type="text" Value="CSE" className="input input-bordered w-full max-w-xs" />
+
+                    <label className="label">
+                        <span className="label-text">I started studying form</span>
+                    </label>
+                    <select className="select select-bordered w-full max-w-xs">
+                        <option disabled selected>
+                            year
+                        </option>
+                        {years.map((year) => (
+                            <option key={year}>{year}</option>
+                        ))}
+                    </select>
+                    <br /><br />
+                    <button className="btn btn-sar btn-sm">Save</button>
+
+                </div>
                 <div className="">
                     <h3 className="text-xl font-semibold">Profile Photo</h3>
                     <hr />
                     <br />
-                    <div className="avatar">
+                    <div className="avatar ">
                         <div className="w-48 rounded">
-                            <img src="https://img.freepik.com/free-photo/waist-up-portrait-handsome-serious-unshaven-male-keeps-hands-together-dressed-dark-blue-shirt-has-talk-with-interlocutor-stands-against-white-wall-self-confident-man-freelancer_273609-16320.jpg?w=996&t=st=1687522239~exp=1687522839~hmac=1953cb0346fb849e096878aa2c8581507ba6591301bd457fe87c4d8ba8209d39" />
+                            <img src="https://frontend-learning-app.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Frakib.081885d9.jpg&w=1080&q=75" />
                         </div>
                     </div>
                     <form>
@@ -47,58 +75,31 @@ const ProfileSettings = () => {
                     </form>
                 </div>
 
+
             </div>
             <br />
 
-
-            <div className="study">
-                <h3 className="text-xl font-semibold">Study</h3>
-                <hr />
-                <label className="label">
-                    <span className="label-text">I study at</span>
-                </label>
-                <input type="text" placeholder="Varendra University" className="input input-bordered w-full max-w-xs" />
-
-                <label className="label">
-                    <span className="label-text">I study in </span>
-                </label>
-                <input type="text" Value="CSE" readOnly className="input input-bordered w-full max-w-xs" />
-
-                <label className="label">
-                    <span className="label-text">I started studying form</span>
-                </label>
-                <select className="select select-secondary w-full max-w-xs">
-                    <option disabled selected>
-                        Pick a year
-                    </option>
-                    {years.map((year) => (
-                        <option key={year}>{year}</option>
-                    ))}
-                </select>
-                <br /><br />
-                <button className="btn btn-sar btn-sm">Save</button>
-
+            <br />
+            <div className="flex flex-col md:flex-row justify-between mt-4">
+                <div className="password">
+                    <h3 className="text-xl font-semibold">Password</h3>
+                    <hr />
+                    <label className="label">
+                        <span className="label-pass">Old Password</span>
+                    </label>
+                    <input type="password" className="input input-bordered w-full max-w-xs" />
+                    <label className="label">
+                        <span className="label-pass">New Password</span>
+                    </label>
+                    <input type="password" className="input input-bordered w-full max-w-xs" />
+                    <label className="label">
+                        <span className="label-pass">confirm Password</span>
+                    </label>
+                    <input type="password" className="input input-bordered w-full max-w-xs" />
+                    <br /><br />
+                    <button className="btn btn-sar btn-sm">Save</button>
+                </div>
             </div>
-            <br /><br />
-            <div className="password">
-                <h3 className="text-xl font-semibold">Password</h3>
-                <hr />
-                <label className="label">
-                    <span className="label-pass">Old Password</span>
-                </label>
-                <input type="password" className="input input-bordered w-full max-w-xs" />
-                <label className="label">
-                    <span className="label-pass">New Password</span>
-                </label>
-                <input type="password" className="input input-bordered w-full max-w-xs" />
-                <label className="label">
-                    <span className="label-pass">confirm Password</span>
-                </label>
-                <input type="password" className="input input-bordered w-full max-w-xs" />
-                <br /><br />
-                <button className="btn btn-sar btn-sm">Save</button>
-            </div>
-
         </div>
     );
 };
