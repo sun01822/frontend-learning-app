5d996beb37c2700a3e22e98467da8f0686f00447
MDShariarHossainSun <mdshariarhossainsun01822@gmail.com>
2023-11-15 19:50:32 +0600

    Add Proposal portion

--- components/apply/Proposal.jsx
+++ components/apply/Proposal.jsx
@@ -1,19 +1,17 @@
-import Link from "next/link";
 import { useGetAllCommentsOnPostQuery } from "@/redux/features/comment/commentApi";
 import moment from "moment";
 
 const Proposal = ({ id }) => {
-  // Fetch comments for the current post using the useGetAllCommentsOnPostQuery hook
-  const { data: comments, isError, error } = useGetAllCommentsOnPostQuery({ postId: id }, {refetchOnMountOrArgChange: true});
-  console.log("Comment: ", comments);
-  console.log("ProblemID: ", id);
+  const { data: comments, isError, error } = useGetAllCommentsOnPostQuery(
+    { postId: id },
+    { refetchOnMountOrArgChange: true }
+  );
 
-  console.log("Error: ", error);
   return (
-    <div className="space-y-4">
-      <h2 className="text-2xl font-semibold mb-4">Proposals</h2>
+    <div className="mt-4 space-y-4">
+      <h2 className="text-2xl font-semibold mt-16 mb-4">Proposals</h2>
 
-      {isError && <p>Error loading proposals</p>}
+      {isError && <p className="text-red-500">Error loading proposals</p>}
 
       {!isError && comments && comments.length === 0 && (
         <p>No proposals available yet.</p>
@@ -23,14 +21,26 @@ const Proposal = ({ id }) => {
         <ul>
           {comments.map((comment) => (
             <li key={comment._id} className="border p-4 rounded-md">
-              <p className="text-gray-700">{comment.proposal}</p>
-              <div className="flex justify-between items-center mt-2">
-                {/* <span className="text-sm text-gray-500">
-                  Submitted {moment(comment.createdAt).fromNow()}
-                </span> */}
-                {/* <Link href={`/profile/${comment.userid}`}>
-                  <a className="text-blue-500">View Profile</a>
-                </Link> */}
+              <div className="flex justify-between items-center mb-2">
+                <div className="flex items-center">
+                  <img
+                    src={comment.userId.image || "/default/avatar.jpg"}
+                    alt={comment.userId.name}
+                    className="w-12 h-12 rounded-full mr-4"
+                  />
+                  <span className="text-lg font-semibold text-blue-500">
+                    {comment.userId.name}
+                  </span>
+                </div>
+                <span className="text-lg font-semibold text-green-500">
+                  {comment.price} tk
+                </span>
+              </div>
+              <p className="text-gray-700 mb-2">{comment.proposal}</p>
+              <div className="flex justify-between items-center">
+                <span className="text-sm text-gray-500">
+                  {moment(comment.createdAt).fromNow()}
+                </span>
               </div>
             </li>
           ))}
