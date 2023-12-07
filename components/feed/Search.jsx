32d7ec5f59cf4aa1c4222cb077334e33fa98b136
mohammedrakib944 <mohammedrakib944@gmail.com>
2023-12-07 11:01:37 +0600

    Comment out

--- components/feed/Search.jsx
+++ components/feed/Search.jsx
@@ -10,7 +10,6 @@ const Search = () => {
           placeholder="Search"
           className="w-full py-3 focus:outline-none px-5 border-r"
         />
-        {/* No chnages */}
         <button className="px-5">
           <AiOutlineSearch />
         </button>
